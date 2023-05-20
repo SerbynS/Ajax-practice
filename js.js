6198d0fc3dc62c21447dbce47c8bd59bac82c760
SerbynS <serbin.sasha1@gmail.com>
2023-05-20 17:54:24 +0300

    ddd

--- js.js
+++ js.js
@@ -21,7 +21,7 @@ xhr.onload = function(){
             <p class="product-price">${p.price}</p>
             <p class="product-description">${p.description}</p>
             <a href="userProfile.html?id=${p.author_id}">Seller profile</a>
-            <button>Buy</button>
+            <button onclick="addProductToCart(${p.id})">Buy</button>
             
         `;
         productsGrid.appendChild(pElem);
@@ -32,3 +32,49 @@ xhr.onload = function(){
 }
 
 xhr.send();
+
+let cartProd = document.getElementById("cart-products");
+
+function openCart(){
+    cartProd.classList.toggle('hide');
+}
+
+let cart = [];
+function addProductToCart(id){
+    let product = productsArray.find(function(p){
+        return p.id == id;
+    });
+    cart.push(product);
+    drawCartProducts();
+}
+
+function drawCartProducts(){
+    if(cart.length === 0) 
+    return cartProd.innerHTML = 'Cart is empty';
+
+    cartProd.innerHTML = null;
+
+    let sum = 0;
+
+    cart.forEach(function(p){
+        cartProd.innerHTML += `
+            <p> <img src="${p.photo_url}"> ${p.name} |${p.price} </p>
+            <hr>
+        `;
+        sum += p.price;
+
+    });
+
+    cartProd.innerHTML += `
+        <p>Total Price: ${sum}</p>
+        <button onclick="buyAll()">Buy All</button>
+    `;
+}
+
+function buyAll(){
+    cart = [];
+    cartProd.innerHTML = "Money was withdraw from your credit card";
+}
+
+
+
--- profile.js
+++ profile.js
@@ -1,4 +1,5 @@
 let profile = document.getElementById("profile");
+let productsGrid = document.getElementById("user-products-grid");
 
 let url = "https://my-json-server.typicode.com/SerbynS/Ajax-practice";
 let xhr = new XMLHttpRequest();
@@ -21,3 +22,29 @@ xhr.onload = function(){
 }
 
 xhr.send();
+
+
+let productsRequest = new XMLHttpRequest();
+
+productsRequest.open("GET", `${url}/products?author_id=${id}`);
+
+productsRequest.responseType = 'json';
+
+productsRequest.onload = function(){
+    let products = productsRequest.response;
+    productsGrid.innerHTML = null;
+    products.forEach(p => {
+        productsGrid.innerHTML += `
+            <div class="product"> 
+            <h2 class="product-name">${p.name}</h2>
+            <img class="product-photo" src="${p.photo_url}">
+            <p class="product-price">${p.price}</p>
+            <p class="product-description">${p.description}</p>
+            </div>
+        `;
+    });
+
+}
+
+
+productsRequest.send();
